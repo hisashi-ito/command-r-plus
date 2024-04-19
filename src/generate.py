84605b51f0e82d579ed3fd6cfa74876e030eda58
Hisashi Itoh <it.hisas@gmail.com>
2024-04-19 23:46:48 +0900

    Update generate.py
    add model info

--- src/generate.py
+++ src/generate.py
@@ -2,6 +2,37 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_id = "CohereForAI/c4ai-command-r-plus-4bit"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+""" model info
+ohereForCausalLM(
+  (model): CohereModel(
+    (embed_tokens): Embedding(256000, 12288, padding_idx=0)
+    (layers): ModuleList(
+      (0-63): 64 x CohereDecoderLayer(
+        (self_attn): CohereAttention(
+          (q_norm): CohereLayerNorm()
+          (k_norm): CohereLayerNorm()
+          (q_proj): Linear4bit(in_features=12288, out_features=12288, bias=False)
+          (k_proj): Linear4bit(in_features=12288, out_features=1024, bias=False)
+          (v_proj): Linear4bit(in_features=12288, out_features=1024, bias=False)
+          (o_proj): Linear4bit(in_features=12288, out_features=12288, bias=False)
+          (rotary_emb): CohereRotaryEmbedding()
+        )
+        (mlp): CohereMLP(
+          (gate_proj): Linear4bit(in_features=12288, out_features=33792, bias=False)
+          (up_proj): Linear4bit(in_features=12288, out_features=33792, bias=False)
+          (down_proj): Linear4bit(in_features=33792, out_features=12288, bias=False)
+          (act_fn): SiLU()
+        )
+        (input_layernorm): CohereLayerNorm()
+      )
+    )
+    (norm): CohereLayerNorm()
+  )
+  (lm_head): Linear(in_features=12288, out_features=256000, bias=False)
+)
+"""
+
 model = AutoModelForCausalLM.from_pretrained(model_id)
 
 # Format message with the command-r-plus chat template
